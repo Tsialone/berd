0463834add821ddddd14f3aa14dfc2aed18647df
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-03-06 22:06:55 +0300

    fix refresh in HomeController

--- src/main/java/com/berd/dev/controllers/HomeController.java
+++ src/main/java/com/berd/dev/controllers/HomeController.java
@@ -1,9 +1,14 @@
 package com.berd.dev.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.berd.dev.models.Unite;
+import com.berd.dev.services.UniteService;
 
 import lombok.RequiredArgsConstructor;
 
