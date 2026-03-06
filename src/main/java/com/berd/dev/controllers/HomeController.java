e3a49913ae9de1e5ba13e1c9df92292913141734
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-03-06 22:06:55 +0300

    fix refresh in HomeController

--- src/main/java/com/berd/dev/controllers/HomeController.java
+++ src/main/java/com/berd/dev/controllers/HomeController.java
@@ -1,21 +1,28 @@
 package com.berd.dev.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.berd.dev.models.Unite;
+import com.berd.dev.services.UniteService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-
+    private final UniteService uniteService;
     @GetMapping ("/")
     public String getListe(Model model) {
 
 
         
+        List  <Unite> unites = uniteService.getAll();
+        System.out.println("refresh: " + unites.size());
         model.addAttribute("content", "pages/home");
         return "admin-layout";
     }
