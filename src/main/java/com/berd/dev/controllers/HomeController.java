eea1ad8e568d63cf123f6aeb1d7ec761107166d0
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-03-06 21:09:24 +0300

    monitoring database too

--- src/main/java/com/berd/dev/controllers/HomeController.java
+++ src/main/java/com/berd/dev/controllers/HomeController.java
@@ -5,17 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.berd.dev.services.UniteService;
+
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final UniteService  uniteService;
+
     @GetMapping ("/")
     public String getListe(Model model) {
 
-
-        
+        uniteService.getAll();
         model.addAttribute("content", "pages/home");
         return "admin-layout";
     }
