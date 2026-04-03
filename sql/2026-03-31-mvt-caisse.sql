3e5152092186bd2a0b0b11b13fe86a5789a5a86d
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-04-03 20:39:45 +0300

    set from release

--- sql/2026-03-31-mvt-caisse.sql
+++ sql/2026-03-31-mvt-caisse.sql
@@ -10,21 +10,7 @@ CREATE TABLE caisse_mvt(
    FOREIGN KEY(id_caisse) REFERENCES caisse(id_caisse)
 );
 
-
-CREATE TABLE caisse(
-   id_caisse SERIAL,
-   nom VARCHAR(255)  NOT NULL,
-   description TEXT,
-   created TIMESTAMP NOT NULL,
-   solde  DOUBLE PRECISION NOT NULL,
-   id_caisse_categorie INTEGER NOT NULL,
-   id_utilisateur INTEGER NOT NULL,
-   PRIMARY KEY(id_caisse),
-   UNIQUE(id_caisse_categorie),
-   FOREIGN KEY(id_caisse_categorie) REFERENCES caisse_categorie(id_caisse_categorie),
-   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
-);
 INSERT INTO caisse (nom, description, created, solde, id_caisse_categorie, id_utilisateur) 
-VALUES ('portefeuille', 'mlay e', NOW  () , 0.0 , 1 , 1);
+VALUES ('portefeuille', 'Portefeuille maison', NOW  () , 0.0 , (SELECT id_caisse_categorie FROM caisse_categorie WHERE libelle = 'wallet') , 1);
 
 ALTER TABLE depense ADD COLUMN montant_total  DOUBLE PRECISION  NOT NULL  DEFAULT 0.0;
