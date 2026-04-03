2d3e32736f993b7c42a65207270045eb3d95d64f
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-04-03 18:55:57 +0300

    update 2026-04-3_2-fix-depense_sql.sql
    remove constraint caisse_id_caisse_categorie_key
    truncate caisse_cateogorie

--- sql/2026-04-3_2-fix-depense_sql.sql
+++ sql/2026-04-3_2-fix-depense_sql.sql
@@ -1,2 +1,9 @@
 ALTER TABLE caisse ALTER  COLUMN id_caisse_categorie SET   NOT NULL ;
 ALTER TABLE caisse DROP CONSTRAINT  caisse_id_caisse_categorie_key  ;
+TRUNCATE TABLE caisse_categorie RESTART IDENTITY CASCADE ;
+ALTER TABLE caisse_categorie ADD CONSTRAINT caisse_categorie_libelle_key   UNIQUE (libelle) ;
+
+INSERT INTO caisse_categorie (libelle , created) VALUES ('wallet' , NOW ());
+INSERT INTO caisse_categorie (libelle , created) VALUES ('mvola' , NOW ());
+INSERT INTO caisse_categorie (libelle , created) VALUES ('orange money' , NOW ());
+INSERT INTO caisse_categorie (libelle , created) VALUES ('banque' , NOW ());
